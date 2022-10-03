35448f2dde97e3b27ed1c656513629ed869aaacb
DEJENERUTA <45858936+DEJENERUTA@users.noreply.github.com>
2022-10-03 11:11:53 +0200

    add route, so that all pages are existed

--- dans-app/src/App.js
+++ dans-app/src/App.js
@@ -1,5 +1,24 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Login from "./pages/Login";
+import Activities from "./pages/Activities";
+import Calander from "./pages/Calander";
+import Search from "./pages/Search";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Welcome />} />
+          <Route path="/activities" element={<Activities />} />
+          <Route path="/calander" element={<Calander />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
